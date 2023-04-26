cd1e32f8c5e655133804fdd14db9fbe05671366d
nissemand243 <47781194+nissemand243@users.noreply.github.com>
2023-04-26 09:49:36 +0200

    Tested and implemented IsLeapYear method

--- Ex0.Tests/UnitTest1.cs
+++ Ex0.Tests/UnitTest1.cs
@@ -1,12 +1,66 @@
+using System;
+using System.IO;
 using Xunit;
+using Ex0;
 
 namespace Ex0.Tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void IsLeapYear_GivenYear1973_returnFalse()
     {
+        //Arrange
+        int input = 1973;
+        Year testObj = new Year(){};
 
+        //act
+        var actual = testObj.IsLeapYear(1973);
+        var expected = false;
+
+        //assert
+        Assert.Equal(actual,expected);
+    }
+    [Fact]
+    public void IsLeapYear_Given1900_returnFalse()
+    {
+        //Arrange
+        int input = 1900;
+        Year testObj = new Year(){};
+
+        //act
+        var actual = testObj.IsLeapYear(input);
+        var expected = false;
+
+        //assert
+        Assert.Equal(actual,expected);
+    }
+    [Fact]
+    public void IsLeapYear_Given1600_returnTrue()
+    {
+        //Arrange
+        int input = 1600;
+        Year testObj = new Year(){};
+
+        //act
+        var actual = testObj.IsLeapYear(input);
+        var expected = true;
+
+        //assert
+        Assert.Equal(actual,expected);
+    }
+    [Fact]
+    public void IsLeapYear_Given1004_returnTrue()
+    {
+        //Arrange
+        int input = 1004;
+        Year testObj = new Year(){};
+
+        //act
+        var actual = testObj.IsLeapYear(input);
+        var expected = true;
+
+        //assert
+        Assert.Equal(actual,expected);
     }
 }
--- Ex0/Program.cs
+++ Ex0/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace Ex0 
+{
+    public class Year
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+
+        public bool IsLeapYear(int year)
+        {
+            if(year % 4 == 0)
+            {
+                if(year % 100 == 0)
+                {
+                    if(year % 400 == 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+// See https://aka.ms/new-console-template for more information
